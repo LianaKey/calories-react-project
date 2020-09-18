5db81eaa1b8106c1c0086ea369338743a0d0971e
Onish <donowan2@gmail.com>
2020-09-18 12:20:10 +0300

    adding loop of card items

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,28 +1,22 @@
 import React from 'react'
 import Card from './Card'
 
-const CardList:React.FC = () => (
-        <div className="row">
+let cardlist:any[] = []
+
+const CardList:React.FC = () => {
+    
+        for (let i:number = 0; i < 10; i++) {
+            cardlist.push(<Card key={i} />)
+        }
+
+        return (
+            <div className="row">
             <h3 className="header">Dishes and products</h3>
             <div className="col s6 m6 cardlist flexlist mt1">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-          </div>
-        </div>
-)
+                {cardlist}
+            </div>
+            </div>
+        )
+}
 
 export default CardList
