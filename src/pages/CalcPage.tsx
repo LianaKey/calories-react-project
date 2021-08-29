fd7e84e236839c7843160564d93c3dfa0c459409
Сергей Ониш <donowan2@gmail.com>
2021-08-29 21:45:00 +0300

    adding toast materialize

--- src/pages/CalcPage.tsx
+++ src/pages/CalcPage.tsx
@@ -2,17 +2,25 @@ import React from 'react'
 import CardList from '../components/card/CardList'
 import TopForm from './../components/form_limit/TopForm'
 import Title from '../components/Title'
+import { Toast } from 'react-materialize';
 
 class CalcPage extends React.Component  {
-    render () {
-      return(
-        <div className="calc">
-            <Title />
-            <TopForm />
-            <CardList />
-        </div>
-      )
-    }
+  render () {
+    return(
+      <div className="calc">
+        <Title />
+        <TopForm />
+        <CardList />
+        <Toast
+          options={{
+            html: 'Here you go!'
+          }}
+        >
+          Toast
+        </Toast>
+      </div>
+    )
+  }
 }
 
 export default CalcPage
