f100046c78b3177f8a5f2f7dbb28a91308385852
Сергей Ониш <donowan2@gmail.com>
2021-09-06 00:29:46 +0300

    add useEffect

--- src/components/calculator/CalcForm.tsx
+++ src/components/calculator/CalcForm.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CalcItem } from './CalcItem'
 import { useSelector } from 'react-redux'
-import { IState } from "../../interfaces"
+import { IState, IDish } from "../../interfaces"
 
-export const LargeForm: React.FC = () => {
+export const CalcForm: React.FC = () => {
 
   const { dishes, dayLimit }: IState = useSelector((state: IState) => state);
-
-  function calc() {
+  const [summ, setSumm] = useState(0);
+  
+  const calc = (items:IDish[]):number => {
     let summ = 0
-    dishes.map((item: any) => (item.calculated) ? summ = summ + item.calories : '')
+    items.map((item: any) => (item.calculated) ? summ = summ + item.calories : '')
     return summ
   }
+
+  useEffect(() => {
+    setSumm(calc(dishes));
+  },[dishes]);
 
   return (
     <div className="col s12">
@@ -21,8 +26,8 @@ export const LargeForm: React.FC = () => {
         })}
       </ul>
       <div className="row margin0 total p2">
-        <span className="to-left">Your day limit: <strong className="f20">{dayLimit}</strong></span>
-        <span className="to-right">In total: <strong className="f20">{calc()}</strong> calories.</span>
+        <span className="to-left">Your day limit: <strong className="f20">{ dayLimit }</strong></span>
+        <span className="to-right">In total: <strong className="f20">{ summ }</strong> calories.</span>
       </div>
     </div>
   )
